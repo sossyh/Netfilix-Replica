ad9b5749675f31f4d09cb8023f5223cc982c467e
sossyh <88205224+sossyh@users.noreply.github.com>
2022-08-16 10:08:28 +0300

    Update detail.js

--- javaScript/detail.js
+++ javaScript/detail.js
@@ -1,8 +1,8 @@
-const coverpostorclass = document.getElementsByClassName("cover")[0];
-coverpostorclass.style.backgroundImage="url(/images/img1.jpg)";
+// const coverpostorclass = document.getElementsByClassName("cover")[0];
+// coverpostorclass.style.backgroundImage="url(/images/img1.jpg)";
 
-fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=4f63a0332df5c38b700db53eb95ee62f').then((data)=>{
-    return data.json();
-}).then((jsondata)=>{
+// fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=4f63a0332df5c38b700db53eb95ee62f').then((data)=>{
+//     return data.json();
+// }).then((jsondata)=>{
     
-})
+// })
